93a920939fdbbf63d2147eac22a3a5a2bba588ae
BricoBC <barajas.c.bruno.n@gmail.com>
2023-12-08 10:56:16 -0600

    Start project

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-page class="flex flex-center">
+
+    <section style="width: 100%;" class="row justify-around bg-primary">
+      <div style="width: 50%;">
+        Hola
+      </div>
+
+      <div style="width: 40%;">
+        <q-img src="../assets/images/Personal.jpg" alt="Foto_personal">
+          <div class="absolute-bottom text-subtitle1 text-center">
+            Hola, soy {{ name }}.
+          </div>
+        </q-img>
+      </div>
+
+    </section>
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { apiFotos } from "src/boot/axios";
+
+export default defineComponent({
+  name: "IndexPage",
+  data() {
+    return {
+      name: "Bruno Nicolas",
+    };
+  },
+  methods: {
+    
+  },
+});
+</script>
